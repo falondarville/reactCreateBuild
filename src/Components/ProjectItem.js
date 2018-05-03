d644df0d6148e0dca312ce7334436e7276ab0810
Falon Darville <falondarville@gmail.com>
2018-05-03 16:36:08 -0700

    add key and print projects

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -3,10 +3,12 @@ import registerServiceWorker from "./../registerServiceWorker";
 
 class ProjectItem extends Component {
   render() {
-    console.log(this.props);
-    // when you return in a render, you can only have one element. For example, everything here is wrapped in a single DIV.
-    // you can't use class as an attribute here, so you must use className
-    return <li className="Projects">My projects.</li>;
+    return (
+      <li className="Project">
+        <strong>{this.props.project.title}</strong>
+        : {this.props.project.category}
+      </li>
+    );
   }
 }
 
--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import registerServiceWorker from "./../registerServiceWorker";
+import ProjectItem from "./ProjectItem";
 
 // we use map in the following because we are iterating through an array
+// add a key to the ProjectItems of you will get an error
 class Projects extends Component {
   render() {
     let projectItems;
     if (this.props.projects) {
       projectItems = this.props.projects.map(project => {
         // console.log(project);
-        return;
+        return <ProjectItem key={project.title} project={project} />;
       });
     }
-    console.log(this.props);
+    // console.log(this.props);
     // when you return in a render, you can only have one element. For example, everything here is wrapped in a single DIV.
-    return <div className="Projects">My projects.</div>;
+    return <div className="Projects">{projectItems}</div>;
   }
 }
 
