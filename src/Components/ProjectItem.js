3a15737c66b46720d5e77540442fbd06fdac1cac
Falon Darville <falondarville@gmail.com>
2018-05-03 16:13:50 -0700

    map projects

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import registerServiceWorker from "./../registerServiceWorker";
 
-class Projects extends Component {
+class ProjectItem extends Component {
   render() {
     console.log(this.props);
     // when you return in a render, you can only have one element. For example, everything here is wrapped in a single DIV.
-    return (
-      <div className="Projects">
-        My projects.
-        {this.props.test}
-      </div>
-    );
+    // you can't use class as an attribute here, so you must use className
+    return <li className="Projects">My projects.</li>;
   }
 }
 
-export default Projects;
+export default ProjectItem;
--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import registerServiceWorker from "./../registerServiceWorker";
 
+// we use map in the following because we are iterating through an array
 class Projects extends Component {
   render() {
+    let projectItems;
+    if (this.props.projects) {
+      projectItems = this.props.projects.map(project => {
+        // console.log(project);
+        return;
+      });
+    }
     console.log(this.props);
     // when you return in a render, you can only have one element. For example, everything here is wrapped in a single DIV.
-    return (
-      <div className="Projects">
-        My projects.
-        {this.props.test}
-      </div>
-    );
+    return <div className="Projects">My projects.</div>;
   }
 }
 
