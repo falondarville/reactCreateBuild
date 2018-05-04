038d2e48e531da3eb05e0ddd2ea016e1a278590a
Falon Darville <falondarville@gmail.com>
2018-05-03 17:54:23 -0700

    add componentWillMount

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import Projects from "./Components/projects";
 import "./App.css";
 import registerServiceWorker from "./registerServiceWorker";
 
+// where you set the state is not usually where you want to house the data, instead add your state as an empty array
+
 class App extends Component {
   constructor() {
     // when adding a constructor in a class, you must first call super function
     super();
     this.state = {
+      projects: []
+    };
+  }
+
+  // lifecycle method: look up all the lifecycle methods on the documentation, they render at different time. For example: componentDidMount
+  // this is where you will add information that you grab through an API
+  componentWillMount() {
+    this.setState({
       projects: [
         {
           title: "Business Website",
@@ -22,8 +32,9 @@ class App extends Component {
           category: "Web Development"
         }
       ]
-    };
+    });
   }
+
   render() {
     // when you return in a render, you can only have one element. For example, everything here is wrapped in a single DIV.
     // import projects to appear below app content
