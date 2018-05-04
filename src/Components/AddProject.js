ed24add50722ff8fc2008f8445eb3d6a7411cb0e
Falon Darville <falondarville@gmail.com>
2018-05-03 18:31:51 -0700

    create dynamic form with categories

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -4,10 +4,32 @@ import registerServiceWorker from "./../registerServiceWorker";
 // we use map in the following because we are iterating through an array
 // add a key to the ProjectItems of you will get an error
 class AddProject extends Component {
+  static defaultProps = {
+    categories: ["Web Design", "Web Development", "Mobile Development"]
+  };
   render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return (
+        <option key={category} value="category">
+          {category}
+        </option>
+      );
+    });
     return (
       <div>
         <h3 Add Project />
+        <form>
+          <div>
+            <label>Title</label>
+            <br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label>Category</label>
+            <br />
+            <select ref="category">{categoryOptions}</select>
+          </div>
+        </form>
       </div>
     );
   }
